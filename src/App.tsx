b9f8023a3582101cbf5f68f728c551af447c7c0c
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-05 22:22:28 +0200

    fix: App.tsx reallocate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import UserManualControl from './components/organisms/UserManualControl'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <div className='w-dvw h-dvh'>
+      <UserManualControl></UserManualControl>
+    </div>
+  )
+}
+
+export default App
