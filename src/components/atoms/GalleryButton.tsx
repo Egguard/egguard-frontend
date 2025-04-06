4a12647545e804c782ae0fb5c8ddfa5e21b2718a
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-06 20:51:01 +0200

    feat: gallery button

---
+++ src/components/atoms/GalleryButton.tsx
@@ -0,0 +1,23 @@
+const GalleryButton = () => {
+  const getLastPicture = () => {
+    // example
+    console.log("Fetching the last picture...");
+    return null;
+  };
+
+  return (
+    <div className="relative w-40 h-32">
+      <img
+        src={getLastPicture() || "/default-img.jpg"}
+        alt="Gallery button"
+        className="w-full h-full border-4 border-white rounded-lg object-cover"
+      />
+      <div
+        className="content-[''] absolute bg-black 
+        w-full h-full -top-2 -left-2 rounded-lg -z-10 border-gray-100 border-4 opacity-70"
+      />
+    </div>
+  );
+};
+
+export default GalleryButton;
