97169296787cd9a7c9d0bd644850242cb66b7cf0
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-06 20:51:25 +0200

    feat: ShutterButton

---
+++ src/components/atoms/ShutterButton.tsx
@@ -0,0 +1,16 @@
+const ShutterButton = () => {
+  const handleClick = () => {
+    console.log("Shutter button clicked");
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="size-20 rounded-full bg-white outline-8 outline-white/40"
+    >
+      Click Me
+    </button>
+  );
+};
+
+export default ShutterButton;
