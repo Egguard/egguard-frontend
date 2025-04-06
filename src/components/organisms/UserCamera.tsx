8432696ef9819469e8b2ca06cd364adc10002f49
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-06 21:47:19 +0200

    fix: camera buttons position

--- src/components/organisms/UserCamera.tsx
+++ src/components/organisms/UserCamera.tsx
@@ -4,11 +4,11 @@ import Gallery from "../molecules/Gallery";
 
 const UserCamera = () => {
   return (
-    <div className="size-full">
+    <div className="size-full bg-red-500 relative">
       {/* Camera div molecule */}
       <CameraFeed />
 
-      <div className="inline-flex items-end justify-between p-6 h-full w-full">
+      <div className="inline-flex absolute bottom-0 left-0 items-end justify-between p-6 w-full">
         {/* Gallery molecule with atoms: gallery button and gallery popup */}
         <Gallery />
 
