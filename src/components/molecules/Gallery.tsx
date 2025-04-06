ab8c64a531bb00c99b7f2df526fce61c69c70f35
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-06 21:35:50 +0200

    feat: Gallery with toggle done

---
+++ src/components/molecules/Gallery.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import GalleryButton from "../atoms/GalleryButton";
+import GalleryPopUp from "../atoms/GalleryPopup";
+
+const Gallery = () => {
+    const[popUp, setPopUp] = useState<boolean>(false);
+
+    const togglePopup = () => {
+        setPopUp(!popUp);
+    }
+
+  return (
+    <div className="relative size-full">
+      <GalleryButton onClick={togglePopup} />
+
+      {popUp && <GalleryPopUp togglePopup={togglePopup} />}
+    </div>
+  );
+};
+
+export default Gallery;
