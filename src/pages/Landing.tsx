98dd217f6fd7bd472b799cad877b503e071ca9e8
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-10 19:48:14 +0200

    feat: Landing page with components
    Opinions yet to be done

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,15 @@
+import BuyEgguard from "../components/organisms/Landing/BuyEgguard";
+import Hero from "../components/organisms/Landing/Hero";
+import WhyUs from "../components/organisms/Landing/WhyUs";
+
+const Landing = () => {
+  return (
+    <>
+      <Hero />
+      <WhyUs />
+      <BuyEgguard />
+    </>
+  );
+};
+
+export default Landing;
