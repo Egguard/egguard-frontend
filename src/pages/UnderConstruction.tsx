68ba011ebade8c18dfa8b06385d74f3aa8e365cd
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-10 22:11:54 +0200

    feat: UnderConstruction page
    for unfinished pages

---
+++ src/pages/UnderConstruction.tsx
@@ -0,0 +1,13 @@
+import Button from "../components/atoms/Button";
+
+const UnderConstruction = () => {
+  return(
+    <div className="flex flex-col justify-center items-center h-screen">
+        <h2>Lo siento, aún estamos trabajando en esto...</h2>
+        <img className="h-80 object-cover" src="https://i.pinimg.com/736x/ce/06/f6/ce06f6dc2e58ee0575b0abe7558773e8.jpg" alt="gatos trabajando" />
+        <Button className="w-96" as="button" onClick={() => window.history.back()}>Volver</Button>
+    </div>
+  );
+};
+
+export default UnderConstruction;
