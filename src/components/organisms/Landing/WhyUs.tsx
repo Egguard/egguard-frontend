68676543967aebafcbf9a92bdb51749cd5520324
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-10 21:36:31 +0200

    fix: WhyUs section size fixes + stock vidoe

--- src/components/organisms/Landing/WhyUs.tsx
+++ src/components/organisms/Landing/WhyUs.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CarrousellButtons from "../../molecules/CarrousellButtons";
 
 const vids = [
   {
-    videoUrl: "https://www.youtube.com/watch?v=vWQpiMd-v0A",
+    videoUrl: "src/assets/images/stock-chickens.mp4",
     text: "Bla bla bla del primero...",
   },
   {
-    videoUrl: "https://www.youtube.com/watch?v=75uDqThKGQ4",
+    videoUrl: "src/assets/images/stock-chickens.mp4",
     text: "Más bla bla del segundo...",
   },
   {
-    videoUrl: "https://www.youtube.com/watch?v=VTFnV14MOsk",
+    videoUrl: "src/assets/images/stock-chickens.mp4",
     text: "Y otro bla bla del tercero...",
   },
 ];
@@ -19,33 +19,35 @@ const vids = [
 const WhyUs = () => {
   const [activeVid, setActiveVid] = useState(0);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActiveVid((prev) => (prev + 1) % 3);
+    }, 7000);
+
+    return () => clearInterval(intervalId);
+  }, [activeVid]);
+
   return (
-    <div className="relative w-dvw h-6/10  px-32 py-8 pb-16 flex flex-col justify-between bg-black overflow-clip">
-      <h2 className="text-white">¿Por qué elegirnos?</h2>
+    <div className="relative w-dvw h-[60dvh] px-32 py-8 pb-16 flex flex-col justify-between bg-black overflow-hidden">
+      <h2 className="text-white text-3xl font-bold relative z-10">¿Por qué elegirnos?</h2>
 
       <div
         className="absolute inset-0 flex transition-transform duration-700 ease-in-out"
         style={{ transform: `translateX(-${activeVid * 100}%)` }}
       >
         {vids.map((item, i) => (
-          <div key={i} className="w-full flex-shrink-0">
-            <video
-              className="w-full h-96 object-cover"
-              src={item.videoUrl}
-              autoPlay
-              loop
-              muted
-            />
-            <p className="text-white text-3xl font-bold ml-32 mb-16">{item.text}</p>
+          <div key={i} className="relative w-full flex-shrink-0">
+            <video className="w-full h-full object-cover brightness-50" src={item.videoUrl} autoPlay loop muted />
+
+            <div className="absolute inset-0 flex items-end pb-16">
+              <p className="text-white text-3xl font-bold ml-32">{item.text}</p>
+            </div>
           </div>
         ))}
       </div>
 
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-4">
-        <CarrousellButtons
-          activeVid={activeVid}
-          onClick={(index) => setActiveVid(index)}
-        />
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-4 z-10">
+        <CarrousellButtons activeVid={activeVid} onClick={(index) => setActiveVid(index)} />
       </div>
     </div>
   );
