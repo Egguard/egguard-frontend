9a0efa2af55fe92965c49629d8dbaf275e24cdea
mluimes <mario.luis.mesa2004@gmail.com>
2025-04-13 23:18:30 +0200

    fix: UnderConstrction height

--- src/pages/UnderConstruction.tsx
+++ src/pages/UnderConstruction.tsx
@@ -6,7 +6,7 @@ interface UnderConstructionInterface {
 
 const UnderConstruction = ({noVolver}: UnderConstructionInterface) => {
   return(
-    <div className="flex flex-col justify-center items-center size-full">
+    <div className={`flex flex-col justify-center items-center size-full ${!noVolver ? 'h-screen' : ''}`}>
         <h2>Lo siento, aún estamos trabajando en esto...</h2>
         <img className="h-80 object-cover" src="https://i.pinimg.com/736x/ce/06/f6/ce06f6dc2e58ee0575b0abe7558773e8.jpg" alt="gatos trabajando" />
         {!noVolver && <Button className="w-96" as="button" onClick={() => window.history.back()}>Volver</Button>}
