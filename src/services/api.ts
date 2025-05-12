0c4c7dcb9b853fb524679dd59ea7aaab85b5074e
mluimes <mario.luis.mesa2004@gmail.com>
2025-05-13 00:33:12 +0200

    feat: complete api call

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,26 @@
-import { UserNotification } from "../types/apiTypes";
+import { PaginatedResponse, UserNotification } from "../types/apiTypes";
 
-const API_BASE_URL = "";
+const API_BASE_URL = "http://localhost:8080";
+const FARM_ID = 1;
 
 // API calls
-export const getUserNotifications = async (): Promise<UserNotification[]> => {
+export const getUserNotifications = async (
+  page = 0, 
+  size = 5
+): Promise<PaginatedResponse<UserNotification>> => {
   try {
-    const response = await fetch(`${API_BASE_URL}/api/v1/notifications`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      `${API_BASE_URL}/api/v1/farms/${FARM_ID}/notifications?page=${page}&size=${size}`, 
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     const data = await response.json();
+    console.log("getUserNotifications: ", data);
 
     if (!response.ok) {
       throw new Error(data.message || "Error fetching user notifications");
--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -1,8 +1,43 @@
+export interface PaginatedResponse<T> {
+  totalElements: number;
+  totalPages: number;
+  pageable: {
+    pageSize: number;
+    pageNumber: number;
+    paged: boolean;
+    unpaged: boolean;
+    offset: number;
+    sort: {
+      unsorted: boolean;
+      sorted: boolean;
+      empty: boolean;
+    }
+  };
+  size: number;
+  content: T[];
+  number: number;
+  sort: {
+    unsorted: boolean;
+    sorted: boolean;
+    empty: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
 export interface UserNotification {
     id: number,
     farmId: number,
-    severity: string;
+    severity: NotificationSeverity;
     message: string;
     photoUrl: string;
-    timestamp: Date;
+    timestamp: string;
+}
+
+export enum NotificationSeverity {
+    CRITICAL = "CRITICAL",
+    WARNING = "WARNING",
+    INFO = "INFO"
 }
