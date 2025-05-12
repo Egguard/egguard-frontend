aeebf692693894e76ceef0787da0113a226cd065
mluimes <mario.luis.mesa2004@gmail.com>
2025-05-12 12:07:08 +0200

    feat: UserNotifications using hook + loading and error screens
    NOTE: fetch is currently wrong, have to rethink it with pagination and stuff

--- src/components/organisms/UserNotifications.tsx
+++ src/components/organisms/UserNotifications.tsx
@@ -1,13 +1,39 @@
 import Notification from "../molecules/Notification";
+import { useUserNotifications } from "../../lib/hooks/useUserNotifications";
 
 const UserNotifications = () => {
+  const { data, isLoading, isError } = useUserNotifications();
+
   return (
-    <div className="size-8/10 mx-auto self-center">
+    <div className="w-8/10 mx-auto py-12">
       <div className="inline-flex justify-between w-full items-center">
         <h2 className="font-bold text-black/60">Notificaciones</h2>
         <div>Paginador</div>
       </div>
-      <Notification severity="danger" title="Hola" date={new Date} img="asd" />
+      <div className="space-y-4 mt-4">
+        {isLoading && (
+          <div className="flex items-center justify-center flex-col mt-40">
+            <p className="text-gray-400 text-2xl font-bold text-center">
+              Cargando...
+            </p>
+            <img className="w-1/4" src="/loader.gif" />
+          </div>
+        )}
+        {isError && (
+          <p className="text-red-500 text-2xl font-bold text-center mt-40">Error al cargar las notificaciones.</p>
+        )}
+        {!isLoading &&
+          !isError &&
+          data?.map((n) => (
+            <Notification
+              key={n.id}
+              severity={n.severity}
+              message={n.message}
+              img={n.photoUrl}
+              date={n.timestamp}
+            />
+          ))}
+      </div>
     </div>
   );
 };
