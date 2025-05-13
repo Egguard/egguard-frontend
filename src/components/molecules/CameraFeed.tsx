a53f83fd47fcef41cf53cddf598f0b3d8185422b
mluimes <mario.luis.mesa2004@gmail.com>
2025-05-13 02:35:19 +0200

    feat: CameraFeed without buttons for dashboard

--- src/components/molecules/CameraFeed.tsx
+++ src/components/molecules/CameraFeed.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+import { LoadingState } from "../atoms/States";
+
 const CameraFeed = () => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
+
+  const handleImageError = () => {
+    setIsImageLoaded(false);
+  };
+
   return (
     <div className="absolute top-0 left-0 size-full -z-10" id="divCamera">
+      {!isImageLoaded && (
+        <div className="flex items-center justify-center size-full bg-gray-200">
+          <LoadingState />
+        </div>
+      )}
       <img
-      className="size-full object-cover"
+        className={`size-full object-cover ${isImageLoaded ? "" : "hidden"}`}
         id="cameraFeed"
         crossOrigin="anonymous"
         src="http://localhost:8080/stream?topic=/camera/image_raw"
+        onLoad={handleImageLoad}
+        onError={handleImageError}
       />
     </div>
   );
